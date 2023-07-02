70d0850a022e289964e88d444094c800ea321f28
Hua Szu Yang <33264733+huaszu@users.noreply.github.com>
2023-07-02 01:14:51 -0400

    Initial commit

---
+++ browse-user-twitter-feed.py
@@ -0,0 +1,64 @@
+import requests
+import os
+import json
+
+# To set environment variables in your terminal, run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+
+API_KEY = os.environ["API_KEY"]
+API_SECRET = os.environ["API_SECRET"]
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
+BEARER_TOKEN = os.environ["BEARER_TOKEN"]
+
+
+def create_url():
+    # Replace with user ID below
+    user_id = 2244994945
+    return f"https://api.twitter.com/2/users/{user_id}/tweets"
+
+
+def get_params():
+    # Tweet fields are adjustable.
+    # Options include:
+    # attachments, author_id, context_annotations,
+    # conversation_id, created_at, entities, geo, id,
+    # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
+    # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
+    # source, text, and withheld
+    return {"tweet.fields": "created_at"}
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {BEARER_TOKEN}"
+    r.headers["User-Agent"] = "v2UserTweetsPython"
+    return r
+
+
+def connect_to_endpoint(url, params):
+
+    # response = requests.get(url=url, params=params)
+
+    response = requests.request("GET", url=url, auth=bearer_oauth, params=params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            f"Request returned an error: {response.status_code} {response.text}"
+            )
+
+    return response.json()
+
+
+def main():
+    url = create_url()
+    params = get_params()
+    json_response = connect_to_endpoint(url, params)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
